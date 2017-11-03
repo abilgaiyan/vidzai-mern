dda45314e2a7c9fe1fe23a171ef80dce0ac0f13f
ajay bilgaiyan <abilgaiyan@gmail.com>
2017-11-03 23:27:47 +0530

    test changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-   res.send({hi: 'there'});
+   res.send({hi: 'hay ho lets go'});
 });
 
 const PORT = process.env.PORT || 5000;
@@ -10,3 +10,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log('listening on port', PORT);
 })
+
+// https://quiet-eyrie-51962.herokuapp.com/
