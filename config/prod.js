3e990f70d966a0ca6a0e64c5f751379a77417b5e
abilgaiyan <abilgaiyan@gmail.com>
2018-04-16 20:59:53 +0530

    config changes store in config file

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@
 module.exports ={
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
+    mongodbURL: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     stripePubilishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
