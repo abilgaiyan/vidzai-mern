c85d8ef13afef1660630e0f7f07565ac065e6046
abilgaiyan <abilgaiyan@gmail.com>
2018-04-26 09:49:25 +0530

    ui changes

--- client/src/components/CustomerStories.js
+++ client/src/components/CustomerStories.js
@@ -31,7 +31,136 @@ Our stories are pervasive across Event invitations, Lead conversion, Campaign La
 
 </div>
 </div>
-     </div>
+
+<div className="container">
+
+<div className="row">
+    <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div className="tt-title">
+            <div className="tt-title-count wow fadeInUp">Title</div>
+            <h3 className="tt-title-name wow fadeInUp">small text</h3>
+            <div className="tt-title-text wow fadeInUp">Our clients are our best assets, the greatest pride! <br />Hear what our customers have to say.</div>
+        </div>
+    </div>
+</div>
+</div>
+<div className="container-fluid">
+                <div className="isotope-content">
+                   
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="200ms">
+                        <div className="tt-page">
+                        <Link to="/">
+                            <div className="tt-page-testimonial text-center">
+                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
+                                <div className="tt-text wow fadeInUp">
+                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+
+                                </div>
+                            </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">Sukino And You</div>
+                               
+                            </div>
+                      </Link>
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="400ms">
+                        <div className="tt-page">
+                        <Link to="/">
+                            <div className="tt-page-testimonial text-center">
+                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
+                                <div className="tt-text wow fadeInUp">
+                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+
+                                </div>
+                            </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">DPS</div>
+                               
+                            </div>
+                      </Link>
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="600ms">
+                        <div className="tt-page">
+                        <Link to="/">
+                            <div className="tt-page-testimonial text-center">
+                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
+                                <div className="tt-text wow fadeInUp">
+                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+
+                                </div>
+                            </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">Retail</div>
+                               
+                            </div>
+                      </Link>
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="800ms">
+                        <div className="tt-page">
+                        <Link to="/">
+                            <div className="tt-page-testimonial text-center">
+                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
+                                <div className="tt-text wow fadeInUp">
+                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+
+                                </div>
+                            </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">Retail</div>
+                               
+                            </div>
+                      </Link>
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="1000ms">
+                        <div className="tt-page">
+                        <Link to="/">
+                            <div className="tt-page-testimonial text-center">
+                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
+                                <div className="tt-text wow fadeInUp">
+                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+
+                                </div>
+                            </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">Retail</div>
+                               
+                            </div>
+                      </Link>
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="1200ms">
+                        <div className="tt-page">
+                        <Link to="/">
+                            <div className="tt-page-testimonial text-center">
+                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
+                                <div className="tt-text wow fadeInUp">
+                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+
+                                </div>
+                            </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">Retail</div>
+                               
+                            </div>
+                      </Link>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                    <div className="col-sm-12 text-center marginTop10 marginBottom20 footerarrow">
+        <p>Like what you see? Try it for yourself</p>
+        <div className="arrow bounce marginTop20">
+          <Link to="/customerstories1"> <i className="fa fa-angle-double-down fa-3x"></i></Link>
+          </div>
+          </div>
+          <div className="clearfix"></div>
+                    </div>
+
+    
       );
     };
     
---
+++ client/src/components/CustomerStories1.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const CustomerStories1 = () => {
+    return (
+        <div>
+       
+       <div className="container-fluid wow fadeInUp marginTop70 marginBottom40">
+<div className="col-sm-5 col-xs-12 youleft">
+<h3>Creative to Mass production - 1 Team + 1 Platform
+ </h3>
+<h1>Your story, your touch,<br /> your way, with Vidzai
+</h1>
+</div>
+<div className="col-sm-7 col-xs-12 youright Agency"><p>
+
+Storyboarded, illustrated, animated by the Vidzai team. Mass produced by the Vidzai platform, our customer stories have a unique blend of expression, data and interactions.
+
+
+
+</p>
+<p>
+
+You can become our Affiliate and use our platform to create these engaging stories for your customers. <br />
+Earn payouts from Sale of Vidzai technology when you promote us.
+
+</p>
+<p>
+
+We will handhold you through entire process right from training, Closing the sales and post sales support 
+
+</p>
+
+
+</div>
+</div>
+<div className="container-fluid">
+                <div className="isotope-content">
+                   
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="200ms">
+                        <div className="tt-page">
+                      
+                        <div class="resp-container">
+						   <iframe class="resp-iframe" src="https://www.youtube.com/embed/CUYPPWmpveQ?rel=0" allow="encrypted-media" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
+						   
+						   </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">A Home Care Story
+</div>
+                               
+                            </div>
+                    
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="400ms">
+                        <div className="tt-page">
+                       
+                        <div class="resp-container">
+						   <iframe class="resp-iframe" src="https://www.youtube.com/embed/CUYPPWmpveQ?rel=0" allow="encrypted-media" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
+						   
+						   </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">An Event Invitation Story</div>
+                               
+                            </div>
+                      
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="600ms">
+                        <div className="tt-page">
+                       
+                        <div class="resp-container">
+						   <iframe class="resp-iframe" src="https://www.youtube.com/embed/CUYPPWmpveQ?rel=0" allow="encrypted-media" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
+						   
+						   </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">A Patient Story
+</div>
+                               
+                            </div>
+                    
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="800ms">
+                        <div className="tt-page">
+                        
+                        <div class="resp-container">
+						   <iframe class="resp-iframe" src="https://www.youtube.com/embed/CUYPPWmpveQ?rel=0" allow="encrypted-media" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
+						   
+						   </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">A Retail Buyer’s Story
+</div>
+                               
+                            </div>
+                   
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="1000ms">
+                        <div className="tt-page">
+                       
+                        <div class="resp-container">
+						   <iframe class="resp-iframe" src="https://www.youtube.com/embed/CUYPPWmpveQ?rel=0" allow="encrypted-media" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
+						   
+						   </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">A Recruitment Story
+</div>
+                               
+                            </div>
+                     
+                        </div>
+                    </div>
+                    <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="1200ms">
+                        <div className="tt-page">
+                        
+                        <div class="resp-container">
+						   <iframe class="resp-iframe" src="https://www.youtube.com/embed/CUYPPWmpveQ?rel=0" allow="encrypted-media" allowfullscreen="" width="100%" height="100%" frameborder="0"></iframe>
+						   
+						   </div>
+                            <div className="tt-page-rating">
+                                <div className="tt-page-title test-author">A Recruitment Story
+</div>
+                               
+                            </div>
+                    
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+       </div>
+      );
+    };
+    
+    export default CustomerStories1;
+       
