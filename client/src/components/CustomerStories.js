babaffb6c11bd0982de69bce02cc2631255d62ec
abilgaiyan <abilgaiyan@gmail.com>
2018-04-26 11:30:51 +0530

    fixed html changes as per react way.

--- client/src/components/CustomerStories.js
+++ client/src/components/CustomerStories.js
@@ -1,168 +1,180 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const CustomerStories = () => {
     return (
-      <div>
-    
-     <div className="container-fluid wow fadeInUp marginTop70 marginBottom40">
-<div className="col-sm-5 col-xs-12 youleft">
-<h3>Generating Results for Early Adopters</h3>
-<h1>Getting personal with your   <br /> customer</h1>
-</div>
-<div className="col-sm-7 col-xs-12 youright Agency"><p>
+        <div>
 
-Our customers are able to leverage the customer data and insights, by playing it back in a engaging manner that traditional communications never could. 
+            <div className="container-fluid wow fadeInUp marginTop70 marginBottom40">
+                <div className="col-sm-5 col-xs-12 youleft">
+                    <h3>Generating Results for Early Adopters</h3>
+                    <h1>Getting personal with your   <br /> customer</h1>
+                </div>
+                <div className="col-sm-7 col-xs-12 youright Agency"><p>
 
-</p>
-<p>
-
-Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
-
+                    Our customers are able to leverage the customer data and insights, by playing it back in a engaging manner that traditional communications never could.
 
 </p>
-<p>
+                    <p>
 
-Our stories are pervasive across Event invitations, Lead conversion, Campaign Launches, Cross selling, Statements and Explainers
+                        Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
 
 
 </p>
+                    <p>
+
+                        Our stories are pervasive across Event invitations, Lead conversion, Campaign Launches, Cross selling, Statements and Explainers
 
 
-</div>
-</div>
+</p>
 
-<div className="container">
 
-<div className="row">
-    <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <div className="tt-title">
-            <div className="tt-title-count wow fadeInUp">Title</div>
-            <h3 className="tt-title-name wow fadeInUp">small text</h3>
-            <div className="tt-title-text wow fadeInUp">Our clients are our best assets, the greatest pride! <br />Hear what our customers have to say.</div>
-        </div>
-    </div>
-</div>
-</div>
-<div className="container-fluid">
+                </div>
+            </div>
+
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <div className="tt-title">
+                            <div className="tt-title-count wow fadeInUp">Title</div>
+                            <h3 className="tt-title-name wow fadeInUp">small text</h3>
+                            <div className="tt-title-text wow fadeInUp">Our clients are our best assets, the greatest pride! <br />Hear what our customers have to say.</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid">
                 <div className="isotope-content">
-                   
+
                     <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="200ms">
                         <div className="tt-page">
-                        <Link to="/">
-                            <div className="tt-page-testimonial text-center">
-                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
-                                <div className="tt-text wow fadeInUp">
-                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+                            <Link to="/">
+                                <div className="tt-page-testimonial text-center">
+                                    <div className="resp-container">
+                                        <iframe className="resp-iframe" src="http://18.216.235.248:3000/api/videourl/clara.mp4" allow="encrypted-media" allowFullScreen="" width="100%" height="100%" frameBorder="0"></iframe>
+                                    </div>
+                                    <div className="tt-text wow fadeInUp">
+                                        Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
 
                                 </div>
-                            </div>
-                            <div className="tt-page-rating">
-                                <div className="tt-page-title test-author">Sukino And You</div>
-                               
-                            </div>
-                      </Link>
+                                </div>
+                                <div className="tt-page-rating">
+                                    <div className="tt-page-title test-author">Sukino And You</div>
+
+                                </div>
+                            </Link>
                         </div>
                     </div>
                     <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="400ms">
                         <div className="tt-page">
-                        <Link to="/">
-                            <div className="tt-page-testimonial text-center">
-                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
-                                <div className="tt-text wow fadeInUp">
-                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+                            <Link to="/">
+                                <div className="tt-page-testimonial text-center">
+                                    <div className="resp-container">
+                                        <iframe className="resp-iframe" src="http://18.216.235.248:3000/api/videourl/clara.mp4" allow="encrypted-media" allowFullScreen="" width="100%" height="100%" frameBorder="0"></iframe>
+
+                                    </div>
+                                    <div className="tt-text wow fadeInUp">
+                                        Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
 
                                 </div>
-                            </div>
-                            <div className="tt-page-rating">
-                                <div className="tt-page-title test-author">DPS</div>
-                               
-                            </div>
-                      </Link>
+                                </div>
+                                <div className="tt-page-rating">
+                                    <div className="tt-page-title test-author">DPS</div>
+
+                                </div>
+                            </Link>
                         </div>
                     </div>
                     <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="600ms">
                         <div className="tt-page">
-                        <Link to="/">
-                            <div className="tt-page-testimonial text-center">
-                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
-                                <div className="tt-text wow fadeInUp">
-                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+                            <Link to="/">
+                                <div className="tt-page-testimonial text-center">
+                                    <div className="resp-container">
+                                        <iframe className="resp-iframe" src="http://18.216.235.248:3000/api/videourl/clara.mp4" allow="encrypted-media" allowFullScreen="" width="100%" height="100%" frameBorder="0"></iframe>
+                                    </div>
+                                    <div className="tt-text wow fadeInUp">
+                                        Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
 
                                 </div>
-                            </div>
-                            <div className="tt-page-rating">
-                                <div className="tt-page-title test-author">Retail</div>
-                               
-                            </div>
-                      </Link>
+                                </div>
+                                <div className="tt-page-rating">
+                                    <div className="tt-page-title test-author">Retail</div>
+
+                                </div>
+                            </Link>
                         </div>
                     </div>
                     <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="800ms">
                         <div className="tt-page">
-                        <Link to="/">
-                            <div className="tt-page-testimonial text-center">
-                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
-                                <div className="tt-text wow fadeInUp">
-                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+                            <Link to="/">
+                                <div className="tt-page-testimonial text-center">
+                                    <div className="resp-container">
+                                        <iframe className="resp-iframe" src="http://18.216.235.248:3000/api/videourl/clara.mp4" allow="encrypted-media" allowFullScreen="" width="100%" height="100%" frameBorder="0"></iframe>
+                                    </div>
+                                    <div className="tt-text wow fadeInUp">
+                                        Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
 
                                 </div>
-                            </div>
-                            <div className="tt-page-rating">
-                                <div className="tt-page-title test-author">Retail</div>
-                               
-                            </div>
-                      </Link>
+                                </div>
+                                <div className="tt-page-rating">
+                                    <div className="tt-page-title test-author">Retail</div>
+
+                                </div>
+                            </Link>
                         </div>
                     </div>
                     <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="1000ms">
                         <div className="tt-page">
-                        <Link to="/">
-                            <div className="tt-page-testimonial text-center">
-                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
-                                <div className="tt-text wow fadeInUp">
-                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+                            <Link to="/">
+                                <div className="tt-page-testimonial text-center">
+                                    <div className="resp-container">
+                                        <iframe className="resp-iframe" src="http://18.216.235.248:3000/api/videourl/clara.mp4" allow="encrypted-media" allowFullScreen="" width="100%" height="100%" frameBorder="0"></iframe>
+                                    </div>
+                                    <div className="tt-text wow fadeInUp">
+                                        Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
 
                                 </div>
-                            </div>
-                            <div className="tt-page-rating">
-                                <div className="tt-page-title test-author">Retail</div>
-                               
-                            </div>
-                      </Link>
+                                </div>
+                                <div className="tt-page-rating">
+                                    <div className="tt-page-title test-author">Retail</div>
+
+                                </div>
+                            </Link>
                         </div>
                     </div>
                     <div className="isotope-item col-xs-6 col-sm-4 wow fadeInUp" data-wow-delay="1200ms">
                         <div className="tt-page">
-                        <Link to="/">
-                            <div className="tt-page-testimonial text-center">
-                                <div className="tt-envato-icon"><img src="../../../img/envato.png" alt="" /></div>
-                                <div className="tt-text wow fadeInUp">
-                                Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
+                            <Link to="/">
+                                <div className="tt-page-testimonial text-center">
+                                    <div className="resp-container">
+                                        <iframe className="resp-iframe" src="http://18.216.235.248:3000/api/videourl/clara.mp4" allow="encrypted-media" allowFullScreen="" width="100%" height="100%" frameBorder="0"></iframe>
+                                    </div>
+                                    <div className="tt-text wow fadeInUp">
+                                        Our applicability is across Healthcare, Education, Retail, Financial Services and other industries.
 
                                 </div>
-                            </div>
-                            <div className="tt-page-rating">
-                                <div className="tt-page-title test-author">Retail</div>
-                               
-                            </div>
-                      </Link>
+                                </div>
+                                <div className="tt-page-rating">
+                                    <div className="tt-page-title test-author">Retail</div>
+
+                                </div>
+                            </Link>
                         </div>
                     </div>
-                    </div>
-                    </div>
-                    <div className="col-sm-12 text-center marginTop10 marginBottom20 footerarrow">
-        <p>Like what you see? Try it for yourself</p>
-        <div className="arrow bounce marginTop20">
-          <Link to="/customerstories1"> <i className="fa fa-angle-double-down fa-3x"></i></Link>
-          </div>
-          </div>
-          <div className="clearfix"></div>
-                    </div>
+                </div>
+            </div>
+            <div className="col-sm-12 text-center marginTop10 marginBottom20 footerarrow">
+                <p>Like what you see? Try it for yourself</p>
+                <div className="arrow bounce marginTop20">
+                    <Link to="/customerstories1"> <i className="fa fa-angle-double-down fa-3x"></i></Link>
+                </div>
+            </div>
+            <div className="clearfix"></div>
+        </div>
 
-    
-      );
-    };
-    
-    export default CustomerStories;
-       
+
+    );
+};
+
+export default CustomerStories;
