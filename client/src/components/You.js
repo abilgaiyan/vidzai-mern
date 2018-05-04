57094c82baad24b6331771dce4b7e9e1e27c9163
abilgaiyan <abilgaiyan@gmail.com>
2018-05-04 17:05:43 +0530

    css html changes

--- client/src/components/You.js
+++ client/src/components/You.js
@@ -12,15 +12,14 @@ const You = () => {
                         <div className="col-sm-6">
                             <div className="tt-banner-info">
                                 <h5 className="tt-banner-label"> <p>Personal Video engages customers</p> </h5>
-                                <h1 className="tt-banner-title">Create an emotional experience for Customer <br /> Her name, her data, her story</h1>
+                                <h1 className="tt-banner-title hidden-xs">Create an emotional experience for Customer <br /> Her name, her data, her story</h1>
+                                <h1 className="tt-banner-title visible-xs hidden-sm hidden-md hidden-lg">Create an emotional experience for Customer Her name, her data, her story</h1>
                             </div>
                         </div>
                         {/* <div> Like what you see? Try it for yourself </div> */}
 						<div className="col-sm-6">
                     <img className="tt-banner-img lazy" data-original="img/banner_01.png" alt="" src="img/banner_01.png" style={{display: "block"}} />
-                    {/* <p>WoW {this.videolist[this.state.counter].name} through a Visual & Personal Story </p> */}
-           {/* <ReactPlayer className="bannervideo"  url= {this.videolist[this.state.counter].videourl}
-              onEnded={() => this.renderNextVideo(this.state.counter + 1)}  playing /> */}
+                 
                 <Typist className="Typist  typewriter">
                    <h6>Did you notice our Team Thanking <br/> customers  like CLARA in the video?</h6>
                    <br/>
